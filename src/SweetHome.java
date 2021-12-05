8c5939c124ad956956230e058a1c7240e5d27a4b
ImNotAntMan <ikarusppong@gmail.com>
2021-12-05 18:09:00 +0900

    2021/12/05 Evening At Home DeskTop

--- src/SweetHome.java
+++ src/SweetHome.java
@@ -1,11 +1,11 @@
-// 집과 관련되 것들을 상속과 다형성 ArrayList를 이용하여 표현할 것.
-// 부모 클래스는 기본적인 Home의 구성 요소 Name, ID, Address, HousePrice, RommQty 
-// 자식 클래스는 BedRoom(잔다, 일어난다), Kitchen(요리, 설거지, 식사), DressRoom(빨래, 말리는)
-// StudyRoom(공부, 영화, 게임)으로 구성
-// 자식 클래스는 오늘 할 일을 구성할 것 어디에서(Bedroom,kitchen...등등)
-// 밥, 옷 갈아입고, 공부하고, 밥먹고, 놀고, 자러간다..
-// 할 일들에 대한 구성은 사람마다 틀리므로 ArrayList에 저장한다.
-import java.util.*;
+// 집과 관련되 것들을 상속과 다형성, ArrayList, 다운 캐스팅을 이용하여 표현할 것.
+// 부모 클래스(SweetHome)는 멤버 변수Name, ID, Address, HousePrice.
+// 자식 클래스는 BedRoom(일어난다), Kitchen(요리, 설거지, 식사), DressRoom(빨래, 말리는),
+// StudyRoom(공부, 영화), GameRoom(게임)으로 구성
+// 자식 클래스는 오늘 할 일을 구성하는데 부모 자식에 있는 homeAction 메소드를 오버라이딩(오버로딩 아 할 것.
+// 일어나서, 밥 먹고, 옷 갈아입고, 공부하고, 논다.(BedRoom, Kitchen, DressRoom, SturyRoom, GameRoom)
+// 할 일들에 대한 구성은 사람마다 틀리므로 ArrayList에 저장한다.(만들었지만 의도에 불분명하다.)
+// 다운 캐스팅을 구현할 수 있도록 자식 클래스에 다른 메소드를 만들어 다운 캐스팅으로 호출할 것.
 public class SweetHome {
 	
 	private String ownerName;	// 집 소유자
@@ -14,6 +14,9 @@ public class SweetHome {
 	private int housePrice;	// 집 가격
 	
 	public SweetHome() {
+		// 빈 생성자(empty constructor)가 없으니까 자식 클래스에서 에러를 낸다. 꼭 만들자!
+		// Implicit super constructor SweetHome() is undefined for default constructor. 
+		// Must define an explicit constructor
 		
 	}
 	
@@ -24,9 +27,14 @@ public class SweetHome {
 		this.housePrice = housePrice;
 	}
 	
-
-	public void homeAction(int i) {
-		System.out.println("집이 움직이는게 아닙니다.");
+	// 오버로딩의 정의는 자바의 한 클래스 내에 이미 사용하려는 이름과 같은 이름을 가진 메소드가 있더라도 매개변수의 개수 또는 타입이 다르면, 같은 이름을 사용해서 메소드를 정의할 수 있다.
+	// 부모 클래스로부터 상속받은 메소드를 자식 클래스에서 재정의하는 것을 오버라이딩이라고 한다.
+	public void homeAction(int i) { // 자식 클래스에서 이 메소드를 오버라이딩한다.
+		System.out.println("SweetHome에서 homeAction()이 호출되었습니다.");
+	}
+	
+	public void taxHouse () {
+		System.out.println("세금은 " + this.getHousePrice()*0.0001*100000000 + "원 입니다.") ;
 	}
 
 	// 일단 set, get을 셋팅하고요
@@ -63,10 +71,13 @@ public class SweetHome {
 	}
 }
 
+
+// 상속을 시작합니다.
 class BedRoom extends SweetHome {
 	
-	public void homeAction(int i) {
-		int selector = i;
+	// 오버라이딩 된 homeAction() 메소드
+	public void homeAction(int i) { // 숫자를 한개 받아서 옵션을 실행하게 끔 한다.
+		int selector = i;	// 부모가 int i 이면 자식도 int i 여야 함.
 		if(selector == 1) {
 			System.out.println("아침에 일찍 일어 났습니다.");
 		} else if(selector == 2) {
@@ -77,9 +88,17 @@ class BedRoom extends SweetHome {
 			System.out.println("잠 안자고 뭐한 겁니까?");
 		}
 	}
+	
+	// 다운 캐스팅 테스트를 위한 자식의 메소드
+	public void bedRoomAction() { 
+		System.out.println("오늘은 휴일입니다.");
+	}
 }
 
+
 class Kitchen extends SweetHome {
+	
+	// 오버라이딩 된 homeAction() 메소드
 	public void homeAction(int i) {
 		int selector = i;
 		if(selector == 1) {
@@ -92,24 +111,38 @@ class Kitchen extends SweetHome {
 			System.out.println("밥 안먹어?");
 		}
 	}
+	
+	// 다운 캐스팅 테스트를 위한 자식의 메소드
+	public void kitchenAction() {
+		System.out.println("오늘은 볶음밥입니다.");
+	}
 }
 
 class DressRoom extends SweetHome {
+	
+	// 오버라이딩 된 homeAction() 메소드
 	public void homeAction(int i) {
 		int selector = i;
 		if(selector == 1) {
 			System.out.println("정장을 입습니다.");
 		} else if(selector == 2) {
-			System.out.println("마스크를 씁니다.");
+			System.out.println("빨래를 열심히 합니다.");
 		} else if (selector == 3) {
-			System.out.println("모자를 씁니다.");
+			System.out.println("빨래를 말립니다.");
 		} else {
 			System.out.println("벗고 나갈려고?");
 		}
 	}
+	
+	// 다운 캐스팅 테스트를 위한 자식의 메소드
+	public void dressRommAction() {
+		System.out.println("정장으로 가자고!!");
+	}
 }
 
 class StudyRoom extends SweetHome {
+	
+	// 오버라이딩 된 homeAction() 메소드
 	public void homeAction(int i) {
 		int selector = i;
 		if(selector == 1) {
@@ -119,24 +152,36 @@ class StudyRoom extends SweetHome {
 		} else if (selector == 3) {
 			System.out.println("영어를 2시간 공부합니다.");
 		} else if (selector == 4) {
-			System.out.println("아침에 일찍 일어났습니다?");
+			System.out.println("영화 조커를 보았습니다.");
 		} else {
 			System.out.println("아침에 게임룸에서 나왔습니다!!");
 		}
 	}
+	
+	// 다운 캐스팅 테스트를 위한 자식의 메소드
+	public void studyRoomAction() {
+		System.out.println("쌀먹충 되는게 좋을까요?");
+	}
 }
 
 class GameRoom extends SweetHome {
+	
+	// 오버라이딩 된 homeAction() 메소드
 	public void homeAction(int i) {
 		int selector = i;
 		if(selector == 1) {
-			System.out.println("리니지 머니를 팔아서 2만원을 벌었습니다.");
+			System.out.println("+9 집행검을 팔아서 2천만원을 벌었습니다.");
 		} else if(selector == 2) {
 			System.out.println("메소를 팔아서 1만원을 벌었습니다.");
 		} else if (selector == 3) {
-			System.out.println("디2리저 아이템을 30만원에 팔았습니다.");
+			System.out.println("디아2리저 아이템을 30만원에 팔았습니다.");
 		} else {
-			System.out.println("잠 안자고 뭐한 겁니까?");
+			System.out.println("인생은 한방입니다.");
 		}
+	}
+	
+	// 다운 캐스팅 테스트를 위한 자식의 메소드
+	public void gameRoomAction() {
+		System.out.println("쌀먹충이 적성이군요!!!!");
 	}
 }
--- src/SweetHomeTest.java
+++ src/SweetHomeTest.java
@@ -2,30 +2,123 @@ import java.util.*;
 public class SweetHomeTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SweetHome sh = new SweetHome("민재기", 1001, "우암동", 100);
+		// 
+		SweetHome sh = new SweetHome("이만기", 1001, "내덕동", 100);
 		ArrayList<SweetHome> shList = new ArrayList<SweetHome>();
+
+		// shList.get()을 통하여 접근하는 방법
+		shList.add(sh);
+//		System.out.println(shList.get(0).getOwnerName());
+//		System.out.println(shList.get(0).getOwnerID());
+//		System.out.println(shList.get(0).getHomeAddress());
+//		System.out.println(shList.get(0).getHousePrice());
+//		System.out.println(shList.get(0).getHousePrice()*0.001);
+		// sh 객체를 통하여 접근하는 방법
+		System.out.print("이름은 " + sh.getOwnerName() + "이고 번호는 " + sh.getOwnerID() + "입니다. " + "사는 곳은 " + sh.getHomeAddress() + "이며 집 값은 " + sh.getHousePrice() + "억원 입니다.\n");
+
 		shList.add(new BedRoom());
 		shList.add(new Kitchen());
 		shList.add(new DressRoom());
 		shList.add(new StudyRoom());
 		shList.add(new GameRoom());
-//		SweetHome bedroom= new BedRoom();
-//		SweetHome kitchenroom = new Kitchen();
-//		SweetHome dressroom = new DressRoom();
-//		SweetHome studyroom = new StudyRoom();
-//		SweetHome gameroom = new GameRoom();
-//		sh.getOwnerName();
-//		sh.getOwnerID();
-//		sh.getHomeAddress();
-//		sh.getHousePrice();
-		System.out.print("이름은 " + sh.getOwnerName() + "이고 번호는 " + sh.getOwnerID() + "입니다. " + "사는 곳은 " + sh.getHomeAddress() + "이며 집값은 " + sh.getHousePrice() + "억원 입니다.\n");
-		for(SweetHome slist : shList) {
-			slist.homeAction(1);
-			slist.homeAction(2);
-			slist.homeAction(3);
-			slist.homeAction(4);
+
+		SweetHome sh2 = new SweetHome("James Lee", 1002, "서울", 250);
+		shList.add(sh2);
+		System.out.print("이름은 " + shList.get(6).getOwnerName() + "이고 번호는 " + shList.get(6).getOwnerID() + "입니다. " + "사는 곳은 " + shList.get(6).getHomeAddress() + "이며 집 값은 " + shList.get(6).getHousePrice() + "억원 입니다.\n");
+		shList.add(new BedRoom());
+		shList.add(new Kitchen());
+		shList.add(new DressRoom());
+		shList.add(new StudyRoom());
+		shList.add(new GameRoom());
+		
+		int j = 0;
+		// 다형성 테스트
+		for(SweetHome slist : shList) { // 이 for문의 단점은 array만 가능.
+			System.out.print(" j의 값은 : " + j + "입니다.");
+			System.out.print(slist.getClass() + " "); // getClass() : 호출된 클래스의 이름을 출력한다.
+//			if(slist instanceof SweetHome) {
+//				System.out.println("");
+//				System.out.print("이름은 " + slist.getOwnerName() + "이고 번호는 " + slist.getOwnerID() + "입니다. " + "사는 곳은 " + slist.getHomeAddress() + "이며 집 값은 " + slist.getHousePrice() + "억원 입니다.\n");			
+//			}
+			slist.homeAction(j); // 다형성: 호출된 클래스의 오버라이딩 된 메소드 homeAction을 실행한다.
+//			slist.homeAction(2); // j의 값을 조정하면 다른 옵션이 출력된다.
+//			slist.homeAction(3);
+//			slist.homeAction(4);
+			++j;
+			if(j == 5) {
+				j = 1;
+			}
 		}
+		
+		System.out.println("================================================");
+		System.out.println("ArrayLis 사용 안 하고~~");
+		System.out.println("================================================");
+		SweetHome bedroom= new BedRoom();	// ArrayList를 사용하지 않는 방법
+		bedroom.homeAction(3); // 사실 이게 맞는 것 같다. 클래스 마다 다른 행동이 가능하니까...
+		SweetHome kitchenroom = new Kitchen(); // 그래서 ArrayList를 사용할까 말까다..
+		kitchenroom.homeAction(2); // 그러면 ArrayList에 매개 변수를 줘서 어떻게 어떻게 하면 안될까?
+		SweetHome dressroom = new DressRoom();
+		dressroom.homeAction(4);
+		SweetHome studyroom = new StudyRoom();
+		studyroom.homeAction(2);
+		SweetHome gameroom = new GameRoom();
+		gameroom.homeAction(4);
+		System.out.println("================================================");
+
+		// 다운 캐스팅 연습!!
+		// super(SweetHome)이 if 문 제일 앞에 오면 다운 캐스팅이 되지 않는다.else 위에 있으면 정상 작동한다. ㅡㅡ;;
+//		for(int i = 0; i < shList.size(); i++) {
+//			SweetHome list = shList.get(i);
+//		 if(list instanceof SweetHome) { // 제일앞에 SweetHome???
+//			System.out.print("이름은 " + shList.get(i).getOwnerName() + "이고 번호는 " + shList.get(i).getOwnerID() + "입니다. " + "사는 곳은 " + shList.get(i).getHomeAddress() + "이며 집 값은 " + shList.get(i).getHousePrice() + "억원 입니다.\n");
+//				list.taxHouse(); // 이게 실행된후에 자식 클래스들은 무조건 .taxHouse()실행함.
+//			} else if(list instanceof BedRoom) {
+//				BedRoom b = (BedRoom)list;
+//				b.bedRoomAction(); // taxHouse() 실행됨...?????
+//			} else if(list instanceof DressRoom) {
+//				DressRoom d = (DressRoom)list;
+//				d.dressRommAction();
+//			} else if(list instanceof Kitchen) {
+//				Kitchen k = (Kitchen)list;
+//				k.kitchenAction();
+//			} else if(list instanceof StudyRoom) {
+//				StudyRoom s = (StudyRoom)list;
+//				s.studyRoomAction();
+//			} else if(list instanceof GameRoom) {
+//				GameRoom g = (GameRoom)list;
+//				g.gameRoomAction();
+//			} else {
+//				System.out.println("알수 없는 오류입니다.");	
+//			}
+//		}
+//		
+		System.out.println("다운 캐스팅 연습!!");
+		System.out.println("================================================");
+		for(int i = 0; i < shList.size(); i++) {
+			SweetHome list = shList.get(i);
+			if(list instanceof BedRoom) {
+				BedRoom b = (BedRoom)list;
+				b.bedRoomAction();
+			} else if(list instanceof DressRoom) {
+				DressRoom d = (DressRoom)list;
+				d.dressRommAction();
+			} else if(list instanceof Kitchen) {
+				Kitchen k = (Kitchen)list;
+				k.kitchenAction();
+			} else if(list instanceof StudyRoom) {
+				StudyRoom s = (StudyRoom)list;
+				s.studyRoomAction();
+			} else if(list instanceof GameRoom) {
+				GameRoom g = (GameRoom)list;
+				g.gameRoomAction();
+				System.out.println("");
+			} else if(list instanceof SweetHome) { // SweetHome: 이름, ID, 주소, 가격이 저장된 클래스
+				System.out.print("이름은 " + list.getOwnerName() + "이고 번호는 " + list.getOwnerID() + "입니다. " + "사는 곳은 " + list.getHomeAddress() + "이며 집 값은 " + list.getHousePrice() + "억원 입니다.\n");
+			} else {
+				System.out.println("알수 없는 오류입니다.");
+			}
+		}
+		
 //		bedroom.homeAction(4);
 //		dressroom.homeAction(1);
 //		kitchenroom.homeAction(3);
@@ -33,7 +126,7 @@ public class SweetHomeTest {
 //		gameroom.homeAction(3);
 //		gameroom.homeAction(1);
 //		gameroom.homeAction(2);
-
+		System.out.println("========================================");
 
 	}
 
